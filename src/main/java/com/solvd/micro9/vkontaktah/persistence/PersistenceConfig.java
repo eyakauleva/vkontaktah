99430f7de1953e6cb38902af54d512ff9c219b71
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-06-19 15:41:08 +0300

    neo4j constraints (enterprise edition)

--- src/main/java/com/solvd/micro9/vkontaktah/persistence/PersistenceConfig.java
+++ src/main/java/com/solvd/micro9/vkontaktah/persistence/PersistenceConfig.java
@@ -1,9 +1,22 @@
 package com.solvd.micro9.vkontaktah.persistence;
 
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.config.EnableNeo4jAuditing;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableNeo4jAuditing
 public class PersistenceConfig {
+
+    final public UserRepository userRepository;
+    final public PostRepository postRepository;
+
+    @PostConstruct
+    public void init() {
+        userRepository.createConstraints();
+        postRepository.createConstraints();
+    }
+
 }
--- src/main/java/com/solvd/micro9/vkontaktah/persistence/UserRepository.java
+++ src/main/java/com/solvd/micro9/vkontaktah/persistence/UserRepository.java
@@ -2,6 +2,21 @@ package com.solvd.micro9.vkontaktah.persistence;
 
 import com.solvd.micro9.vkontaktah.domain.User;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
 
 public interface UserRepository extends Neo4jRepository<User, String> {
+
+    @Query("CREATE CONSTRAINT user_login_required_unique IF NOT EXISTS "
+                + "FOR (user:User) REQUIRE user.login IS NODE KEY; "
+            + "CREATE CONSTRAINT user_firstname_required IF NOT EXISTS "
+                + "FOR (user:User) REQUIRE user.firstName IS NOT NULL; "
+            + "CREATE CONSTRAINT user_lastname_required IF NOT EXISTS "
+                + "FOR (user:User) REQUIRE user.lastName IS NOT NULL; "
+            + "CREATE CONSTRAINT user_age_float IF NOT EXISTS "
+                + "FOR (user:User) REQUIRE user.age IS :: FLOAT; ")
+    void createConstraints();
+
+//    @Query("")
+//    void createIndexes();
+
 }
