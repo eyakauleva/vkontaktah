1150e3d7e172d69004caa560ef75f5ef0ac08a04
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-06-21 13:22:05 +0300

    UserRepository integration testing

--- src/test/java/com/solvd/micro9/vkontaktah/integration/neo4j/UserRepositoryIT.java
+++ src/test/java/com/solvd/micro9/vkontaktah/integration/neo4j/UserRepositoryIT.java
@@ -1,40 +1,83 @@
 package com.solvd.micro9.vkontaktah.integration.neo4j;
 
+import com.solvd.micro9.vkontaktah.domain.Gender;
 import com.solvd.micro9.vkontaktah.domain.User;
 import com.solvd.micro9.vkontaktah.persistence.UserRepository;
-import lombok.SneakyThrows;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.neo4j.core.Neo4jTemplate;
 
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest(classes = Neo4ITConfig.class)
 public class UserRepositoryIT extends Neo4jTestcontainers {
 
-    private static Neo4jTemplate neo4jTemplate;
-
     @Autowired
     private UserRepository userRepository;
 
-    @BeforeAll
-    @SneakyThrows
-    static void init() {
-        String filesList = NEO4J_CONTAINER.execInContainer("ls","/var/lib/neo4j/db_init/").getStdout();
-        System.out.println("---10: " + filesList);
-
-        String filesList2 = NEO4J_CONTAINER.execInContainer("ls","/var/lib/neo4j/conf/").getStdout();
-        System.out.println("---11: " + filesList2);
+    @ParameterizedTest(name = "verifyAllUsersAreReceived [size={0}]")
+    @ValueSource(ints = {3})
+    void verifyAllUsersAreReceived(final int size) {
+        List<User> users = userRepository.findAll();
+        Assertions.assertNotNull(users);
+        Assertions.assertEquals(size, users.size());
     }
 
-    @Test
-    void verifyAllUsersAreReceived() {
-        List<User> users = userRepository.findAll();
-        System.out.println("---100: " + users);
-        Assertions.assertTrue(users.size() > 0);
+    @ParameterizedTest(
+            name = "verifyUserIsFoundById "
+                    + "[id={0},login={1},firstName={2},lastName={3},gender={4},age={5}]"
+    )
+    @CsvSource({
+            "10, liza123, Liza, Ya, FEMALE, 20"
+    })
+    void verifyUserIsFoundById(
+            final String id, final String login, final String firstName,
+            final String lastName, final Gender gender, final int age
+    ) {
+        Optional<User> user = userRepository.findById(id);
+        Assertions.assertNotNull(user);
+        Assertions.assertTrue(user.isPresent());
+        Assertions.assertEquals(user.get().getId(), id);
+        Assertions.assertEquals(user.get().getLogin(), login);
+        Assertions.assertEquals(user.get().getFirstName(), firstName);
+        Assertions.assertEquals(user.get().getLastName(), lastName);
+        Assertions.assertEquals(user.get().getGender(), gender);
+        Assertions.assertEquals(user.get().getAge(), age);
+    }
+
+    @ParameterizedTest(name = "verifyUserIsNotFoundById [id={0}]")
+    @ValueSource(strings = {"634243HJGDGSD12"})
+    void verifyUserIsNotFoundById(final String id) {
+        Optional<User> user = userRepository.findById(id);
+        Assertions.assertNotNull(user);
+        Assertions.assertFalse(user.isPresent());
+    }
+
+    @ParameterizedTest(
+            name = "verifyUserIsSaved "
+                    + "[id={0},login={1},firstName={2},lastName={3},gender={4},age={5}]"
+    )
+    @CsvSource({
+            "1489283, newUser, Petr, Petrov, MALE, 31"
+    })
+    void verifyUserIsSaved(
+            final String id, final String login, final String firstName,
+            final String lastName, final Gender gender, final int age
+    ) {
+        User user = User.builder()
+                .id(id)
+                .login(login)
+                .firstName(firstName)
+                .lastName(lastName)
+                .gender(gender)
+                .age(age)
+                .build();
+        userRepository.save(user);
+        this.verifyUserIsFoundById(id, login, firstName, lastName, gender, age);
     }
 
 }
