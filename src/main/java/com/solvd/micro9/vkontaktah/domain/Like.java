d7738deb54af91865af1032549d4c047a4937f7c
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-06-28 12:55:58 +0300

    NoArgsConstructor added

--- src/main/java/com/solvd/micro9/vkontaktah/domain/Like.java
+++ src/main/java/com/solvd/micro9/vkontaktah/domain/Like.java
@@ -1,7 +1,9 @@
 package com.solvd.micro9.vkontaktah.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.neo4j.core.schema.RelationshipId;
 import org.springframework.data.neo4j.core.schema.RelationshipProperties;
 import org.springframework.data.neo4j.core.schema.TargetNode;
@@ -9,6 +11,8 @@ import org.springframework.data.neo4j.core.schema.TargetNode;
 @RelationshipProperties
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Like {
 
     @RelationshipId
--- src/main/java/com/solvd/micro9/vkontaktah/domain/User.java
+++ src/main/java/com/solvd/micro9/vkontaktah/domain/User.java
@@ -1,7 +1,9 @@
 package com.solvd.micro9.vkontaktah.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Version;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
@@ -9,6 +11,8 @@ import org.springframework.data.neo4j.core.schema.Node;
 @Node
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
